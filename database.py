e4e6fb516c297b3afcf0cacd18043e01c11af78f
George-Vasileiou <75139968+George-Vasileiou@users.noreply.github.com>
2021-06-19 10:31:24 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+import tkinter as tk
 
 
 username = "boss"
@@ -6,8 +7,24 @@ password = "1234"
 con = sql.connect("database.db")
 cur = con.cursor()
 statement = f"SELECT username from users WHERE username='{username}' AND Password = '{password}';"
-cur.execute(statement)
-if not cur.fetchone():  # An empty result evaluates to False.
-    print("Login failed")
-else:
-    print("Welcome")
+
+def login():
+ cur.execute(statement)
+ if not cur.fetchone():  # An empty result evaluates to False.
+     print("Login failed")
+ else:
+     print("Welcome")
+
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+e1 = tk.Entry()
+e2 = tk.Entry()
+
+slogan = tk.Button(frame,
+                   text="Log in",
+                   command=login)
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
